d2af58648007b12ac6d10e9340bdb68c4d18df9d
John Cheng <lego04081996@gmail.com>
2017-08-31 15:48:41 +1200

    DutyCycle variable and PA2 PWM attempts
    added duty cycle and trying to add pa2 pwm

--- Smart-Fan-Controller/GccApplication1/main.c
+++ Smart-Fan-Controller/GccApplication1/main.c
@@ -10,34 +10,42 @@
 
 int main(void)
 {
-    DDRA |= (1<< DDA1);
+    DDRA |= (1 << DDA1) | (1<< DDA2); //set PA1 and PA2 as output (to drive the switches) 
+	DDRA &= ~(1 << DDA3); //set PA3 as input (for hall-effect sensor) 
 
-	TCCR0B |= (1<< CS00);
+	TCCR0B |= (1<< CS00); // clkI/O/(No prescaling)
 
-	// Setting Port A to output
-	//PORTA |= (1<<PORTA1);
+	TCCR1B |= (1<< CS00); // clkI/O/(No prescaling)
 	
-
-	// Fast PWM Mode
-	TCCR0A |=  ((1<< WGM01) | (1<< WGM00));
-	
+	// turn on Fast PWM Mode
+	TCCR0A |= ((1<< WGM01) | (1<< WGM00));
 	TCCR0B |= (1<< WGM02);
+
+	TCCR1A |= ((1<< WGM11) | (1<< WGM10));
+	TCCR1B |= ((1<< WGM12) | (1<< WGM13));
 
 
 	// Set Values for Duty Cycle and Frequency
 	uint8_t TOP = 250;
-	uint8_t dutySet = 125;
+	float Duty = 0.5;
 	
 	OCR0A = TOP;
-	OCR0B = dutySet;
+	OCR0B = TOP*Duty;
 
-	TOCPMSA0 &= ~((1<< TOCC0S0) | (1<< TOCC0S1));
+	OCR1A = TOP;
+	OCR1B = TOP*Duty;
+
+	TOCPMSA0 &= ~((1<< TOCC0S0) | (1<< TOCC0S1)); //what is this for?
+
+	TOCPMSA0 &= ~((1<< TOCC1S0) | (1<< TOCC1S1));
 
 	// Clear OC0B when Compare Match
 	TCCR0A |= (1<< COM0B1);
-	
 	TOCPMCOE |= (1<< TOCC0OE);
-	
+
+	TCCR1A |= (1<< COM1B1); 
+	TOCPMCOE |= (1<< TOCC1OE);
+
     while (1) 
     {
 		
