ef5731e9cadb7572f53cf4051f13fe5d8405f09c
John Cheng <lego04081996@gmail.com>
2017-08-31 16:03:18 +1200

    2nd attempts on PA2 PWM
    haven't test it out yet

--- Smart-Fan-Controller/GccApplication1/main.c
+++ Smart-Fan-Controller/GccApplication1/main.c
@@ -10,20 +10,15 @@
 
 int main(void)
 {
+    uint8_t PA1Value;
     DDRA |= (1 << DDA1) | (1<< DDA2); //set PA1 and PA2 as output (to drive the switches) 
 	DDRA &= ~(1 << DDA3); //set PA3 as input (for hall-effect sensor) 
 
 	TCCR0B |= (1<< CS00); // clkI/O/(No prescaling)
-
-	TCCR1B |= (1<< CS00); // clkI/O/(No prescaling)
 	
 	// turn on Fast PWM Mode
 	TCCR0A |= ((1<< WGM01) | (1<< WGM00));
 	TCCR0B |= (1<< WGM02);
-
-	TCCR1A |= ((1<< WGM11) | (1<< WGM10));
-	TCCR1B |= ((1<< WGM12) | (1<< WGM13));
-
 
 	// Set Values for Duty Cycle and Frequency
 	uint8_t TOP = 250;
@@ -32,23 +27,21 @@ int main(void)
 	OCR0A = TOP;
 	OCR0B = TOP*Duty;
 
-	OCR1A = TOP;
-	OCR1B = TOP*Duty;
-
 	TOCPMSA0 &= ~((1<< TOCC0S0) | (1<< TOCC0S1)); //what is this for?
-
-	TOCPMSA0 &= ~((1<< TOCC1S0) | (1<< TOCC1S1));
 
 	// Clear OC0B when Compare Match
 	TCCR0A |= (1<< COM0B1);
 	TOCPMCOE |= (1<< TOCC0OE);
 
-	TCCR1A |= (1<< COM1B1); 
-	TOCPMCOE |= (1<< TOCC1OE);
 
     while (1) 
     {
-		
+        PA1Value = (PINA1 & (1 << PA1)) ? 1 : 0; //to know whether PA1 is high or not
+        if (value == 1){ //if PA1 is high
+            PORTA &= ~(1 << PORTA2); //then PA2 is low
+        } else { //if PA1 is low
+            PORTA |= (1 << PORTA2); //then PA2 is high
+        }
     }
 }
 
