3e96172374f0bf77bb811003fa82e0c62c897e94
John Cheng <lego04081996@gmail.com>
2017-08-31 18:22:06 +1200

    PWM from PA1&PA3
    1. adjusted code to output from PA1 and PA3
    2. OC0B is routed to TOCC0 (PA1) & TOCC2 (PA3)
    3. When hall sensor is high, set PA1 output to 0, when hall sensor is low, set PA3

--- Smart-Fan-Controller/GccApplication1/main.c
+++ Smart-Fan-Controller/GccApplication1/main.c
@@ -10,20 +10,14 @@
 
 int main(void)
 {
-    DDRA |= (1 << DDA1) | (1<< DDA2); //set PA1 and PA2 as output (to drive the switches) 
-	DDRA &= ~(1 << DDA3); //set PA3 as input (for hall-effect sensor) 
+    DDRA |= (1 << DDA1) | (1<< DDA3); //set PA1 and PA3 as output (to drive the switches) 
+	DDRA &= ~(1 << DDA2); //set PA2 as input (for hall-effect sensor) 
 
 	TCCR0B |= (1<< CS00); // clkI/O/(No prescaling)
-
-	TCCR1B |= (1<< CS00); // clkI/O/(No prescaling)
 	
 	// turn on Fast PWM Mode
 	TCCR0A |= ((1<< WGM01) | (1<< WGM00));
 	TCCR0B |= (1<< WGM02);
-
-	TCCR1A |= ((1<< WGM11) | (1<< WGM10));
-	TCCR1B |= ((1<< WGM12) | (1<< WGM13));
-
 
 	// Set Values for Duty Cycle and Frequency
 	uint8_t TOP = 250;
@@ -32,23 +26,23 @@ int main(void)
 	OCR0A = TOP;
 	OCR0B = TOP*Duty;
 
-	OCR1A = TOP;
-	OCR1B = TOP*Duty;
+	TOCPMSA0 &= ~((1<< TOCC0S0) | (1<< TOCC0S1)); //routes the output of OC0B to TOCC0 (which corresponds PA1)
 
-	TOCPMSA0 &= ~((1<< TOCC0S0) | (1<< TOCC0S1)); //what is this for?
-
-	TOCPMSA0 &= ~((1<< TOCC1S0) | (1<< TOCC1S1));
+	//routes the output of OC0B to TOCC2 (which corresponds PA3)
+	TOCPMSA0 &= ~((1<< TOCC2S0) | (1<< TOCC2S1));
 
 	// Clear OC0B when Compare Match
 	TCCR0A |= (1<< COM0B1);
 	TOCPMCOE |= (1<< TOCC0OE);
 
-	TCCR1A |= (1<< COM1B1); 
-	TOCPMCOE |= (1<< TOCC1OE);
-
     while (1) 
     {
-		
+		if (PINA2 & (1 << PORTA2)) { //read hall sensor signal, if high
+			//turn on one of the switch
+			PORTA &= ~(1 << PORTA1); //turn off PA1
+			} else { //if hall sensor signal is low
+			PORTA &= ~(1 << PORTA3); //turn off PA3
+			}
     }
 }
 
